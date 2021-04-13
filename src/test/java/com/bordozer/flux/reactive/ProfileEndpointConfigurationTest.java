379ded519cbf359e2526dc7a63a3d27729b0a1c3
Boris Lukianov <boris.lukianov@seniordev.com>
2021-04-13 15:25:07 +0300

    routes tests are working

--- src/test/java/com/bordozer/flux/reactive/ProfileEndpointConfigurationTest.java
+++ src/test/java/com/bordozer/flux/reactive/ProfileEndpointConfigurationTest.java
@@ -2,10 +2,12 @@ package com.bordozer.flux.reactive;
 
 import com.bordozer.flux.dto.ProfileDto;
 import com.bordozer.flux.service.ProfileService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -14,17 +16,14 @@ import reactor.core.publisher.Mono;
 import java.util.Collections;
 import java.util.List;
 
-import static com.bordozer.flux.ProfileWebClient.BASE_URL;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProfileEndpointConfigurationTest {
 
-    private static final WebTestClient WEB_TEST_CLIENT = WebTestClient
-            .bindToServer()
-            .baseUrl(BASE_URL)
-            .build();
+    private static WebTestClient WEB_TEST_CLIENT;
 
     private static final ProfileDto PROFILE = ProfileDto.builder()
             .id(1023L)
@@ -33,6 +32,17 @@ class ProfileEndpointConfigurationTest {
 
     @MockBean
     private ProfileService profileService;
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    void setup() {
+        WEB_TEST_CLIENT = WebTestClient
+                .bindToServer()
+                .baseUrl(String.format("http://localhost:%s", randomPort))
+                .build();
+    }
 
     @Test
     public void shouldReturnProfiles() {
@@ -61,5 +71,21 @@ class ProfileEndpointConfigurationTest {
                 .isOk()
                 .expectBody(ProfileDto.class)
                 .isEqualTo(PROFILE);
+    }
+
+    @Test
+    public void shouldCreateProfile() {
+        // given
+        when(profileService.create(any(ProfileDto.class))).thenReturn(Mono.just(PROFILE));
+
+        // when
+        WEB_TEST_CLIENT.post()
+                .uri("/route/profiles")
+                .body(Mono.just(PROFILE), ProfileDto.class)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody()
+                .isEmpty();
     }
 }
