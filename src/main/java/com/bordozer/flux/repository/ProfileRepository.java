61ccac0c157ec0dafcfd806d56861d98d2eec74f
Boris Lukianov <boris.lukianov@seniordev.com>
2021-04-13 17:14:48 +0300

    r2 spring 2

--- src/main/java/com/bordozer/flux/repository/ProfileRepository.java
+++ src/main/java/com/bordozer/flux/repository/ProfileRepository.java
@@ -2,9 +2,7 @@ package com.bordozer.flux.repository;
 
 import com.bordozer.flux.entity.ProfileEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface ProfileRepository extends ReactiveCrudRepository<ProfileEntity, Long> {
 
 }
